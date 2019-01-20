0162dbedd1c6f3041169bebd37690fd0251295c0
Anton Levchenko <anton.levchenko@arcadia.spb.ru>
2019-01-20 13:53:04 +0300

    Task 2 is done.

--- mysql/user.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER 'user1'@'localhost' IDENTIFIED BY 'pass1';
-CREATE USER 'user1'@'%' IDENTIFIED BY 'pass1';
-GRANT ALL ON *.* TO 'user1'@'localhost';
-GRANT ALL ON *.* TO 'user1'@'%';
-FLUSH PRIVILEGES;
